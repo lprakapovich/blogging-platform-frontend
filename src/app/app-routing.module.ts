78f6dc81de344525c536a812d073207acebdd9a1
Liza <audlisab@gmail.com>
2021-12-10 16:43:54 +0100

    add basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {SignUpComponent} from "./sign-up/sign-up.component";
+import {LogInComponent} from "./log-in/log-in.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "about", component: AboutComponent},
+  {path: "home", component: HomeComponent},
+  {path: "signup", component: SignUpComponent},
+  {path: "login", component: LogInComponent},
+  {path: "", redirectTo: "/home", pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    SignUpComponent,
+    LogInComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
