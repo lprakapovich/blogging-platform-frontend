9a9c861e6b31ae878d02ce531b568e0278618bd2
Liza <audlisab@gmail.com>
2022-02-22 13:36:22 +0300

    clean up

--- src/app/components/blog/editor/editor/editor.component.ts
+++ src/app/components/blog/editor/editor/editor.component.ts
@@ -1,5 +1,4 @@
-import {Component, ComponentFactoryResolver, ComponentRef, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
-import {EditorService} from "../../../../services/editor.service";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-editor',
@@ -8,17 +7,10 @@ import {EditorService} from "../../../../services/editor.service";
 })
 export class EditorComponent {
 
-  @ViewChild('parent', {read: ViewContainerRef}) target!: ViewContainerRef;
-  private componentRef!: ComponentRef<any>;
-
   innerHeader = "header"
   innerHtml =
     "<p> I am going to start my first paragraph</p>" +
     "<p> I am going to start my second paragraph</p>";
-
-  constructor(private componentFactoryResolver: ComponentFactoryResolver,
-              private editorService: EditorService) {
-  }
 
   releaseInputOnEnterKeyDown(componentId: string) {
     if (componentId == 'header-span') {
